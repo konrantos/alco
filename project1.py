5aa2afcbae1205a751708f938f1307c1a1a66786
Konstantinos Rantos <148658149+konrantos@users.noreply.github.com>
2023-10-25 15:06:51 +0300

    Update project1.py

--- project1.py
+++ project1.py
@@ -27,15 +27,6 @@ for _ in range(1_000_000):
     # Δημιουργία ενός tuple που περιλαμβάνει το ζευγάρι (αριθμός κάρτας, ποσό) και προσθήκη του στην λίστα charges.Σε κάθε επανάληψη προστίθεται και ένα νέο ζευγάρι στην λίστα.
     charges.append((random_number, random_amount))
     
-# Ενδεικτική εμφάνιση των 5 πρώτων χρεώσεων.
-for i in range(5):
-    # Δημιουργία μεταβλητής first_charges που αποθηκε΄θει τις πρώτες i τυχαίες χρεώσεις της λίστας charges.
-    first_charges = charges[i]
-    # Σπάσιμο του tuple σε δύο μεταβλητές, τις random_number και random_amount. 
-    random_number, random_amount = first_charges
-    # Εκτύπωση των χρεώσων.
-    print(f"Χρέωση {i+1}: Αριθμός: {random_number}, Ποσό: {random_amount} ευρώ.")
-
 # Δημιουργία ενός DataFrame με 2 στήλες από τη λίστα charges.
 charges_df = pd.DataFrame(charges, columns=['Αριθμός Κάρτας', 'Ποσό Χρέωσης'])
 
@@ -58,10 +49,10 @@ max_transaction_count = charges_df['Αριθμός Κάρτας'].value_counts()
 elapsed_time = time.time() - start
 
 # Εμφάνιση των αποτελεσμάτων μαζί με τα ποσά πληρωμών και το πλήθος συναλλαγών.
-print(f"Κάρτα με το μικρότερο συνολικό ποσό πληρωμών: {min_total_payment_card}, Ποσό: {min_total_payment} ευρώ.")
-print(f"Κάρτα με το μεγαλύτερο συνολικό ποσό πληρωμών: {max_total_payment_card}, Ποσό: {max_total_payment} ευρώ.")
-print(f"Κάρτα με το μικρότερο πλήθος συναλλαγών: {min_transaction_count_card}, Πλήθος: {min_transaction_count} χρεώσεις.")
-print(f"Κάρτα με το μεγαλύτερο πλήθος συναλλαγών: {max_transaction_count_card}, Πλήθος: {max_transaction_count} χρεώσεις.")
+print(f"Η κάρτα με το μικρότερο συνολικό ποσό πληρωμών είναι η {min_total_payment_card} με ποσό {min_total_payment} ευρώ.")
+print(f"Η κάρτα με το μεγαλύτερο συνολικό ποσό πληρωμών είναι η {max_total_payment_card} με ποσό {max_total_payment} ευρώ.")
+print(f"Η κάρτα με το μικρότερο πλήθος συναλλαγών είναι η {min_transaction_count_card} με {min_transaction_count} χρεώσεις.")
+print(f"Η κάρτα με το μεγαλύτερο πλήθος συναλλαγών είναι η {max_transaction_count_card} με {max_transaction_count} χρεώσεις.")
 
 # Εμφάνιση συνολικού χρόνου λειτουργιών.
-print(f"Χρόνος εκτέλεσης: {elapsed_time:.2f} δευτερόλεπτα.")
+print(f"Ο συνολικός χρόνος εκτέλεσης είναι {elapsed_time:.2f} δευτερόλεπτα.")
