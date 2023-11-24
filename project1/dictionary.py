f1f84647cda95f6dbf1a7fda3d61087a93e8ca7c
Konstantinos Rantos <148658149+konrantos@users.noreply.github.com>
2023-11-25 01:02:50 +0200

    Update dictionary.py

--- project1/dictionary.py
+++ project1/dictionary.py
@@ -103,8 +103,33 @@ class Linear_Probing_Hash_Table:
         self.table[index] = (key, value)
         # Ενημέρωση του συντελεστή φόρτωσης.
         self.load_factor += 1 / self.size
-
-
+        
+        
+        
+    
+    # Συνάρτηση αφαίρεσης ενός κλειδιού από τον πίνακα κατακερματισμού.
+    def remove(self, key):
+        index = self.hash_function(key)
+        # Η while εκτελείται όσο η θέση του πίνακα έχει κάποιο στοιχείο σε αυτή τη θέση.
+        while self.table[index] is not None:
+            # Έλεγχος για αν το κλειδί βρίσκεται στην τρέχουσα θέση.
+            if self.table[index][0] == key:
+                # Αν ναι, γίνεται αφαίρεση του ζευγαριού κλειδί-τιμής θέτοντας το περιεχόμενο σε None.
+                self.table[index] = None
+                # Ενημέρωση του συντελεστή φόρτωσης μετά την αφαίρεση.
+                self.load_factor -= 1 / self.size
+                # Επιστροφή True για επιτυχία.
+                return True  
+            # Έλεγχος αν η επόμενη θέση είναι κενή προτού μετακινηθούμε σε αυτήν.
+            if self.table[index] is None:
+            # Επιστροφή False αν το κλειδί δε βρέθηκε.
+                return False 
+            # Μετακίνηση στην επόμενη θέση χρησιμοποιώντας γραμμική διερεύνηση.
+            index = (index + 1) % self.size
+            # Επιστροφή False αν το κλειδί δε βρέθηκε.
+            return False  
+    
+    
 
     # Συνάρτηση αναζήτησης ενός κλειδού απο τον πίνακα κατακερματισμού.
     def get(self, key):
@@ -161,8 +186,9 @@ class Linear_Probing_Hash_Table:
                     index = (index + 1) % new_size
                 # Το στοιχείο τοποθετείται στην εντοπισμένη κενή θέση του νέου πίνακα, βάζοντας το tuple (key, value).
                 new_table[index] = (key, value)
-        # Ενημέρωση του μεγέθους και του πίνακα κατακερματισμού και του load factor.
+                # Αυξάνεται ο μετρητής για κάθε πραγματικό στοιχείο.
+                count += 1
+        # Ενημέρωση του μεγέθους, του πίνακα κατακερματισμού και του load factor.
         self.size = new_size
         self.table = new_table
-
-
+        self.load_factor = count / new_size
