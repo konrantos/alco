fd32a38703c51feb690c86a9e36fbaa0c03b7ad5
Konstantinos Rantos <148658149+konrantos@users.noreply.github.com>
2023-11-30 00:04:16 +0200

    Update final_project.py

--- project1/final_project.py
+++ project1/final_project.py
@@ -46,7 +46,7 @@ max_transaction_count_card = max(card_charges, key=lambda card: card_charges[car
 elapsed_time = time.time() - start
 
 # Εμφάνιση των αποτελεσμάτων μαζί με τα ποσά πληρωμών και το πλήθος συναλλαγών.
-print("Αποτελέσματα χρησιμοποιώντας dictionary:")
+print("Αποτελέσματα, χρησιμοποιώντας dictionary:")
 print(f"Η κάρτα με το μικρότερο συνολικό ποσό πληρωμών είναι η {min_total_payment_card} με ποσό {card_charges[min_total_payment_card][0]} ευρώ.")
 print(f"Η κάρτα με το μεγαλύτερο συνολικό ποσό πληρωμών είναι η {max_total_payment_card} με ποσό {card_charges[max_total_payment_card][0]} ευρώ.")
 print(f"Η κάρτα με το μικρότερο πλήθος συναλλαγών είναι η {min_transaction_count_card} με {card_charges[min_transaction_count_card][1]} χρεώσεις.")
@@ -173,11 +173,17 @@ class Linear_Probing_Hash_Table:
         
         
         
-#Δημιουργία αντικειμένου της κλάσης Linear_Probing_Hash_Table.
+# Δημιουργία αντικειμένου της κλάσης Linear_Probing_Hash_Table.
 hash_table = Linear_Probing_Hash_Table()
+# Πλήθος χρεώσεων.
+Ν=10_000
 
 # Εκτέλεση των ίδιων λειτουργιών όπως και με το λεξικό αλλά με χρήση του πίνακα κατακερματισμού.
-for _ in range(1_000_000):
+for _ in range(20_000):
+    random_number = random.randint(0000000000000000, 9999999999999999)
+    numbers.append(random_number)
+
+for _ in range(Ν): 
     random_number = random.choice(numbers)
     random_amount = random.randint(10, 1000)
     
@@ -194,24 +200,20 @@ for _ in range(1_000_000):
          # Αυξάνουμε το πλήθος των συναλλαγών κατά 1.
         card_info[1] += 1
 
-
 # Υπολογισμός και εκτύπωση των αποτελεσμάτων και του αντίστοιχου χρόνου χρησιμοποιώντας τον πίνακα κατακερματισμού.
 start = time.time()
 
-# Φιλτράρισμα του πίνακα κατακερματισμού για τα μη-κενά στοιχεία.
-filtered_table = [item for item in hash_table.table if item is not None]
+min_total_payment_card_table = min(hash_table.table, key=lambda item: item[1][0] if item is not None else float('inf'))
+max_total_payment_card_table = max(hash_table.table, key=lambda item: item[1][0] if item is not None else float('-inf'))
+min_transaction_count_card_table = min(hash_table.table, key=lambda item: item[1][1] if item is not None else float('inf'))
+max_transaction_count_card_table = max(hash_table.table, key=lambda item: item[1][1] if item is not None else float('-inf'))
 
-min_total_payment_card_table = min(filtered_table, key=lambda item: item[1][0])
-max_total_payment_card_table = max(filtered_table, key=lambda item: item[1][0])
-min_transaction_count_card_table = min(filtered_table, key=lambda item: item[1][1])
-max_transaction_count_card_table = max(filtered_table, key=lambda item: item[1][1])
+elapsed_time_table = time.time() - start
 
-elapsed_time = time.time() - start
+print(f"\nΑποτελέσματα, χρησιμοποιώντας τον πίνακα κατακερματισμού για {Ν} χρεώσεις πιστωτικών καρτών:")
+print(f"Η κάρτα με το μικρότερο συνολικό ποσό πληρωμών είναι η {min_total_payment_card_table[0]} με ποσό {min_total_payment_card_table[1][0]} ευρώ.")
+print(f"Η κάρτα με το μεγαλύτερο συνολικό ποσό πληρωμών είναι η {max_total_payment_card_table[0]} με ποσό {max_total_payment_card_table[1][0]} ευρώ.")
+print(f"Η κάρτα με το μικρότερο πλήθος συναλλαγών είναι η {min_transaction_count_card_table[0]} με {min_transaction_count_card_table[1][1]} χρεώσεις.")
+print(f"Η κάρτα με το μεγαλύτερο πλήθος συναλλαγών είναι η {max_transaction_count_card_table[0]} με {max_transaction_count_card_table[1][1]} χρεώσεις.")
 
-print("\nΑποτελέσματα χρησιμοποιώντας τον πίνακα κατακερματισμού:")
-print(f"Κάρτα με το μικρότερο συνολικό ποσό πληρωμών: {min_total_payment_card_table[0]} με ποσό {min_total_payment_card_table[1][0]} ευρώ.")
-print(f"Κάρτα με το μεγαλύτερο συνολικό ποσό πληρωμών: {max_total_payment_card_table[0]} με ποσό {max_total_payment_card_table[1][0]} ευρώ.")
-print(f"Κάρτα με το μικρότερο πλήθος συναλλαγών: {min_transaction_count_card_table[0]} με {min_transaction_count_card_table[1][1]} χρεώσεις.")
-print(f"Κάρτα με το μεγαλύτερο πλήθος συναλλαγών: {max_transaction_count_card_table[0]} με {max_transaction_count_card_table[1][1]} χρεώσεις.")
-
-print(f"Ο συνολικός χρόνος εκτέλεσης είναι {elapsed_time:.2f} δευτερόλεπτα.")
+print(f"Ο συνολικός χρόνος εκτέλεσης είναι {elapsed_time_table:.2f} δευτερόλεπτα.")
